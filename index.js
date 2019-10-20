4ca47dece7fa95ed056375dbcaea247f0f844874
anhquoc1104 <anhquoc1104@gmail.com>
2019-10-20 17:49:18 +0700

    jquery

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //re-build
+let express = require("express");
+let NodeEx = express();
+
+NodeEx.use(express.static("public"));
+NodeEx.set("view engine", "ejs");
+NodeEx.set("views", "./views");
+
+var server = require("http").Server(NodeEx);
+var io = require("socket.io")(server);
+server.listen(8080);
+
+io.on("connection", function(socket){
+	console.log("connect socket: " + socket.id);
+});
+
+NodeEx.get("/", function(req, res){
+	res.render("Home");
+});
